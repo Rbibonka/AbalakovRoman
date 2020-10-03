e3ca13745f8bfcaf235628d1befad0748b09faca
Rbibonka <mamakao.oop@gmail.com>
2020-10-03 16:42:33 +0700

    Release v1

---
+++ MathR.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomaMath
+{
+    public class MathR
+    {
+        public static double addition(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber + SecondNumber;
+        }
+        public static double difference(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber - SecondNumber;
+        }
+        public static double multiplication(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber * SecondNumber;
+        }
+        public static double division(double FirstNumber, double SecondNumber)
+        {
+            if (SecondNumber == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return FirstNumber / SecondNumber;
+            }
+        }
+        public static double areaCircle(double Radius)
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public static double areaSquare(double Side)
+        {
+            return Math.Pow(Side, 2);
+        }
+        public static double areaParallelogram(double Footing, double Height)
+        {
+            return Footing * Height;
+        }
+    }
+}
